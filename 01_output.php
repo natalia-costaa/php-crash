35de4e9cb38c44e890681e6cd3d4d9b4559348e2
natalia-costaa <ncalbuq@gmail.com>
2023-11-20 13:33:06 -0300

    Initial commit

---
+++ 01_output.php
@@ -0,0 +1,17 @@
+<?php
+// echo - Output strings, numbers, html, etc
+    echo 123, "Hello", 10.8;
+
+// print - Works like echo, but can only take in a single argument
+    print 456;
+
+// print_r() - print single values and arrays
+    print_r([1,2,3]);
+
+// var_dump() - Returns more info like data typer and lenght
+    var_dump(true);
+// var_export() - Similar to var_dump(). Outputs a string representation
+//of a variable
+    var_export('hello');
+
+    echo "Lua";
