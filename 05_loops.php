178af1796228c89e3cf665cf5b240071a6058a6f
natalia-costaa <ncalbuq@gmail.com>
2023-11-22 15:04:08 -0300

    Update 05_loops.php

--- 05_loops.php
+++ 05_loops.php
@@ -1,13 +1,42 @@
 <?php
-/* ------------ For Loop ------------ */
+/* ------------ For Loop ------------ 
 
-/*
-** For Loop Syntax
-  for (initialize; condition; increment) {
-  // code to be executed
-  }
-*/
 for($x = 0; $x <= 10; $x++) {
     echo 'Number ' . $x . '<br>';
 }
+*/
 
+/* ------------ While Loop ------------ 
+$x = 1;
+
+while($x <= 15) {
+    echo 'Number ' . $x . '<br>';
+    $x = $x + 1;
+}
+*/
+
+/*
+** Do While Loop Syntax
+$x = 1;
+
+do {
+    echo "Number:" . $x . "<br>";
+    $x++;
+  } while ($x <= 5);
+*/
+$posts = ['First', 'Second', 'Third'];
+
+foreach($posts as $index => $post) {
+    echo $index + 1 . ' - '. $post . '<br>';
+}
+// Use the keys within the loop for an associative array
+$person = [
+    'first_name' => 'Brad',
+    'last_name' => 'Traversy',
+    'email' => 'brad@gmail.com',
+  ];
+  
+  // Get Keys
+  foreach ($person as $key => $val) {
+    echo "${key} - ${val} <br>";
+  }
