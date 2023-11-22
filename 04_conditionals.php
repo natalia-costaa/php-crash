2bff4e2b110d89e82388533b8d0cc44bff42228b
natalia-costaa <ncalbuq@gmail.com>
2023-11-21 22:38:16 -0300

    Create 04_conditionals.php

---
+++ 04_conditionals.php
@@ -0,0 +1,32 @@
+<?php
+
+/* ---- Conditionals & Operators ---- */
+
+/* ------------ Operators ----------- */
+
+/*
+  < Less than
+  > Greater than
+  <= Less than or equal to
+  >= Greater than or equal to
+  == Equal to
+  === Identical to
+  != Not equal to
+  !== Not identical to
+*/
+
+/* ---------- If Statements --------- */
+/*
+** If Statement Syntax
+if (condition) {
+  // code to be executed if condition is true
+}
+*/
+
+    $age = 20;
+
+    if($age >= 18) {
+        echo 'You ate old enough to vote';
+    } else {
+        echo 'Sorry, you are not old enough to vote';
+    };
