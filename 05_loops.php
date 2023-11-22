c2bfc69a5146ae209dc79b330c1f04d6bb8bee24
natalia-costaa <ncalbuq@gmail.com>
2023-11-22 14:35:52 -0300

    Create 05_loops.php

---
+++ 05_loops.php
@@ -0,0 +1,13 @@
+<?php
+/* ------------ For Loop ------------ */
+
+/*
+** For Loop Syntax
+  for (initialize; condition; increment) {
+  // code to be executed
+  }
+*/
+for($x = 0; $x <= 10; $x++) {
+    echo 'Number ' . $x . '<br>';
+}
+
