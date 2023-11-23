6aa63b5bfb9df603aedc5a92a5f6ecfdfd3177c6
natalia-costaa <ncalbuq@gmail.com>
2023-11-23 17:35:02 -0300

    Update 07_arrays_functions.php

--- 07_arrays_functions.php
+++ 07_arrays_functions.php
@@ -11,6 +11,10 @@ var_dump(in_array('apple', $fruits));
 // Add in array
 $fruits[] = 'grape';
 array_push($fruits, 'blueberry', 'strawberry');
+array_unshift($fruits, 'mango');
 
+// Remove in array
+array_pop($fruits);
+array_shift($fruits);
 
 print_r($fruits);
