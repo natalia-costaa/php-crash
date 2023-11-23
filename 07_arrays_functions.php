6a1b3b9ada319086069647589e1512aec3fc3a0c
natalia-costaa <ncalbuq@gmail.com>
2023-11-23 18:31:01 -0300

    Update 07_arrays_functions.php

--- 07_arrays_functions.php
+++ 07_arrays_functions.php
@@ -16,5 +16,18 @@ array_unshift($fruits, 'mango');
 // Remove in array
 array_pop($fruits);
 array_shift($fruits);
+// unset($fruits[2]);
 
-print_r($fruits);
+// Split into 2 chunks
+$chunked_array = array_chunk($fruits,3);
+
+print_r($chunked_array);
+
+// Concatenar arrays
+$arr1 = [1,2,3];
+$arr2 = [7,8,9];
+
+$arr3 = array_merge($arr1, $arr2);
+
+print_r($arr3);
+
