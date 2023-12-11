f638c99d257c24f79aa4d718f92f7dcbbd81568d
natalia-costaa <ncalbuq@gmail.com>
2023-12-11 15:35:19 -0300

    Update 17_oop.php

--- 17_oop.php
+++ 17_oop.php
@@ -1,16 +1,23 @@
 <?php
 class User {
     // Propertires are atributtes that belong to a class
-    public $name;
+   // Access Modifiers: public, private, protected
+  // public - can be accessed from anywhere
+  // private - can only be accessed from inside the class
+  // protected - can only be accessed from inside the class and by inheriting classes
+    private $name;
     public $email;
     public $password;
 
-    
+    //Method is a function that belongs to a class
+    function set_name($name) {
+        $this->name = $name;
+    }
 }
 
 //Instatiate a user object
 $user1 = new User();
 
-$user1->name = 'Brad';
+$user1->set_name('Brad');
 
-echo $user1->name;
+var_dump($user1);
