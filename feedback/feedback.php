771aba13d734a2717c9200ed70cc6ff71f046d39
natalia-costaa <ncalbuq@gmail.com>
2023-12-11 19:31:04 -0300

    Update feedback.php

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -1,23 +1,43 @@
 <?php include 'inc/header.php'; ?>
 
-<h2>Feedback</h2>
+<?php
+  $feedback = [
+    [
+      'id' => 1,
+      'name' => 'Beth Williams',
+      'email' => 'beth@gmail.com',
+      'body' => 'Traversy is ok',
+    ],
+    [
+      'id' => 2,
+      'name' => 'Walt Williams',
+      'email' => 'walt@gmail.com',
+      'body' => 'Traversy sucks',
+    ],
+    [
+      'id' => 3,
+      'name' => 'Bill Johnson',
+      'email' => 'bill@gmail.com',
+      'body' => 'Taversy rocks',
+    ]
+  ];
+?>
 
-    <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-    </div>
-    
-    <div class="card my-3">
-      <div class="card-body">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+<h2>Past Feedback</h2>
+
+<?php if(empty($feedback)): ?>
+  <p class="lead mt3">There is no feedback</p>
+<?php endif; ?>
+
+<?php foreach($feedback as $item): ?>
+    <div class="card my-3 w-75">
+     <div class="card-body text-center">
+      <?php echo $item['body']; ?>
+      <div class="text-secondary mt-2">
+        By <?php echo $item['name']; ?>
+      </div>
       </div>
     </div>
-    
-    <div class="card my-3">
-      <div class="card-body">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-      </div>
-  </div>
+  <?php endforeach; ?>
 
 <?php include 'inc/footer.php'; ?>
