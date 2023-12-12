56c220083cf6f0ff665dae972296b0db37a89649
natalia-costaa <ncalbuq@gmail.com>
2023-12-11 21:11:50 -0300

    Update index.php

--- feedback/index.php
+++ feedback/index.php
@@ -1,12 +1,60 @@
 <?php include 'inc/header.php';?>
 
+<?php
+  $name = $email = $body = '';
+  $nameErr = $emailErr = $bodyErr ='';
+
+  //Form submit
+  if(isset($_POST['submit'])) {
+    if (empty($_POST['name'])) {
+      $nameErr = 'Name is required';
+    } else {
+      $name = filter_input(
+        INPUT_POST,
+        'name',
+        FILTER_SANITIZE_FULL_SPECIAL_CHARS
+      );
+    }
+
+     // Validate email
+    if (empty($_POST['email'])) {
+      $emailErr = 'Email is required';
+    } else {
+      $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    }
+
+    // Validate body
+    if (empty($_POST['body'])) {
+      $bodyErr = 'Body is required';
+    } else {
+      $body = filter_input(INPUT_POST,'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+
+    if (empty($nameErr) && empty($emailErr) && empty($bodyErr)) {
+      // add to database
+      $sql = "INSERT INTO feedback (name, email, body) VALUES ('$name', '$email', '$body')";
+    if (mysqli_query($conn, $sql)) {
+      // success
+      header('Location: feedback.php');
+    } else {
+      // error
+      echo 'Error: ' . mysqli_error($conn);
+    }
+  }
+}
+
+?>
+
 <img src="/php-crash/feedback/img/logo.png" class="w-25 mb-3" alt="">
     <h2>Feedback</h2>
     <p class="lead text-center">Leave feedback for Traversy Media</p>
-    <form action="" class="mt-4 w-75">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="mt-4 w-75">
       <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+        <input type="text" class="form-control is-invalid" id="name" name="name" placeholder="Enter your name">
+        <div id="validationServerFeedback" class="invalid-feedback">
+          Please provide a valid name.
+        </div>
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Email</label>
